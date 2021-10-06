3a4804f296a4ab660fb0add601201d1c6cfb3e72
AntonyOnScript <santhiagoafk@gmail.com>
2021-10-06 20:13:54 -0300

    Login requirement middleware was made!

--- middlewares/global.js
+++ middlewares/global.js
@@ -3,6 +3,7 @@ exports.global = (request, response, next) => {
     response.locals.errors = request.flash('errors')
     response.locals.success = request.flash('success')
     response.locals.user = request.session.user
+    console.log(request.session.user)
 
     next()
 
@@ -23,5 +24,19 @@ exports.csrfToken = (request, response, next) => {
     response.locals.csrfToken = request.csrfToken()
 
     next()
+
+}
+
+exports.checkUser = (request, response, next) => {
+
+    if(request.session.user) {
+        next()
+        return
+    }
+
+    request.flash('errors', 'You must be logged')
+    request.session.save(() => {
+        return response.redirect(process.env.url+'/login')
+    })
 
 }
--- routes.js
+++ routes.js
@@ -5,15 +5,20 @@ const Home = require('./controllers/Home')
 const Contact = require('./controllers/Contact')
 const Login = require('./controllers/Login')
 
+const { checkUser } = require('./middlewares/global')
+
 router.get('/', Home.index)
+
 router.get('/login', Login.index)
-router.get('/logout', Login.logout)
 router.post('/login', Login.login)
+
+router.get('/logout', checkUser,Login.logout)
 router.post('/sign-up', Login.signUp)
-router.get('/', Contact.index)
-router.get('/contact', Contact.index)
-router.post('/contact', Contact.register)
-router.get('/contact/:id', Contact.loadContactById)
-router.post('/contact/update/:id', Contact.editContactById)
+
+router.get('/contact', checkUser, Contact.index)
+router.post('/contact', checkUser, Contact.register)
+
+router.get('/contact/:id', checkUser,Contact.loadContactById)
+router.post('/contact/update/:id', checkUser,Contact.editContactById)
 
 module.exports = router
