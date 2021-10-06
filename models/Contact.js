5fb5a9195ab464bd7384d35f93dc73e052f89340
AntonyOnScript <santhiagoafk@gmail.com>
2021-10-06 20:31:06 -0300

    Database now keeps user id for each contact that is saved

--- models/Contact.js
+++ models/Contact.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const contactSchema = new mongoose.Schema({
+    userId: {type: String, required: true},
     name: String,
     email: String,
     telephone: String,
